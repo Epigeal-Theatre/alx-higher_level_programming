a2ead2900b040736485ab856aa6b6a1b7feb7c17
Epigeal-Theatre <danielonyinkwa1@gmail.com>
2024-04-13 18:59:22 -0700

    even more corrections

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,9 +4,10 @@
 
 import MySQLdb
 import sys
-import argv
+
 
 if __name__ == "__main__":
+    """"access to db"""
     db = MySQLdb.connect(host ="localhost",user =sys.argv[1],
             passwd=sys.argv[2],db=sys.argv[3],port = 3306)
 
